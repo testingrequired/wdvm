ba7cd20e3a0baf344d4da332ea3a77eaad5537c4
Kylee Tilley <kyleetilley@gmail.com>
2018-10-27 19:21:36 -0500

    Implement webdriverDownloader

--- src/cli.ts
+++ src/cli.ts
@@ -1,12 +1,22 @@
 import { mapVersionToDriver } from "./chromedriverVersionMapper";
+import webdriverDownloader from "./webdriverDownloader";
 
-const main = (pkg: any, ...args: string[]) => {
+const main = (pkg: any, args: string[]) => {
   const [browser, version] = args;
+
+  const webdriverVersion = parseFloat(
+    mapVersionToDriver(parseInt(version, 10))
+  );
+
   console.log(`wdvm ${pkg.version}`);
   console.log(`Using ${browser} v${version}`);
-  console.log(
-    `Webdriver version required: ${mapVersionToDriver(parseInt(version, 10))}`
-  );
+  console.log(`Webdriver version required: ${webdriverVersion}`);
+
+  const url = `https://chromedriver.storage.googleapis.com/${webdriverVersion}/chromedriver_win32.zip`;
+
+  webdriverDownloader(url, `chromedriver_win32.${webdriverVersion}.zip`)
+    .then(console.log)
+    .catch(console.log);
 };
 
-main(require("../package.json"), ...process.argv.slice(2));
+main(require("../package.json"), process.argv.slice(2));
